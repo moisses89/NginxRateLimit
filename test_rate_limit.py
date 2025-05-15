2d347482bd6cf035421b947b0a41fb92a939cb26
moisses89 <7888669+moisses89@users.noreply.github.com>
2025-05-15 17:28:07 +0200

    Fix statistics

--- test_rate_limit.py
+++ test_rate_limit.py
@@ -4,23 +4,24 @@ import time
 from collections import defaultdict
 import sys
 
+MAX_ITERATIONS = 5
 
 url = "http://localhost:8080/"
 
 api_keys = [
-    "api_key_1",  
+    "api_key_1",
     "api_key_2",
-    "api_key_3",
+    "api_key_3"
 ]
 
 response_counts = defaultdict(lambda: {"200_OK": 0, "429_TOO_MANY_REQUESTS": 0})
 
 async def send_requests(session, api_key, req_sec):
-    for i in range(0,5):
+    for i in range(0,MAX_ITERATIONS):
         tasks = []
         for _ in range(req_sec):
-            tasks.append(send_request(session, api_key))  
-        await asyncio.gather(*tasks)  
+            tasks.append(send_request(session, api_key))
+        await asyncio.gather(*tasks)
         await asyncio.sleep(1)
 
 async def send_request(session, api_key):
@@ -34,14 +35,12 @@ async def send_request(session, api_key):
         elif response.status == 429:
             response_counts[api_key]["429_TOO_MANY_REQUESTS"] += 1
 
-        print(f"Request time: {current_time_str} Response for {api_key}: {response.status}")
+        print(f"Request time: {current_time_str} Response for {api_key[-5:]}: {response.status}")
 
-def print_statistics(start: int, req_sec: int):
-    end = time.time()
-    test_time = round(end - start)
+def print_statistics(req_sec: int):
     print("\nStatistics:")
     for api_key, counts in response_counts.items():
-        print(f"API Key: {api_key} {req_sec}/s -> Success req/s: {counts['200_OK']/test_time}, error req/s: {counts['429_TOO_MANY_REQUESTS']/test_time}")
+        print(f"API Key: {api_key[-5:]} {req_sec}/s -> Success rate: {counts['200_OK']/MAX_ITERATIONS} req/s, error rate {counts['429_TOO_MANY_REQUESTS']/MAX_ITERATIONS} req/s")
 
 
 
@@ -59,7 +58,6 @@ if __name__ == "__main__":
         req_sec = int(sys.argv[1])
     else:
         req_sec = 5
-    start = time.time()
     asyncio.run(main(req_sec))
     print("\nExiting... Printing statistics")
-    print_statistics(start, req_sec)
+    print_statistics(req_sec)
